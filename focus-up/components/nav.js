5d9add1fb09a880193d653bf501522efb98c55e9
Richard Stephen <g00382880@atu.ie>
2022-10-20 18:45:36 +0100

    Added code to create top and side navigation bars for the homepage

---
+++ focus-up/components/nav.js
@@ -0,0 +1,11 @@
+export default function Nav(){
+    return(
+        <div>
+        <nav>
+            <h1>            
+                FOCUS UP!
+            </h1> 
+        </nav>
+        </div>
+    )
+}
