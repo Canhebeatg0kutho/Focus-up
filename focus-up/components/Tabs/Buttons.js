7d5e53673e70039904309ddfca539b0393222e85
Richard Stephen <g00382880@atu.ie>
2022-11-18 11:14:36 +0000

    Created a [notesId] file for dynamic routing

--- focus-up/components/Tabs/Buttons.js
+++ focus-up/components/Tabs/Buttons.js
@@ -1,5 +1,7 @@
 import classes from "./buttons.module.css"
 import Link from 'next/link';
+import notesSpec from "../../pages/notes/[notesId]"
+
 
 export default function Buttons(){
     return(
@@ -7,7 +9,7 @@ export default function Buttons(){
         <button><Link href = '/tasks'> Tasks </Link></button>
         <button><Link href = '/timer'> Timer </Link></button>
         <button><Link href = '/topics'> Topics </Link></button>
-        <button><Link href = '/calendar'>Notes</Link></button>
+        <button><Link href = '/notes/rtos'>Notes</Link></button>
     </div>
     )
 }
