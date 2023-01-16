d46744b436f9051b53769430f0656c3e2f0aa8c2
Canhebeatg0kutho <G00382880@atu.ie>
2023-01-16 14:16:59 +0000

    Added sign up button to login page

---
+++ focus-up/components/WelcomeButtons/signup.js
@@ -0,0 +1,11 @@
+import classes from "./buttons.module.css"
+
+export default function Signup(){
+    return(
+        <div>
+            <button className={classes.signup}>
+                Sign Up
+            </button>
+        </div>
+    )
+}
--- focus-up/pages/index.js
+++ focus-up/pages/index.js
@@ -1,4 +1,6 @@
 import classes from "../styles/Home.module.css"
+import Logo from "../components/Logo/logo"
+import Signup from "../components/WelcomeButtons/signup"
 const pic = new URL("../images/study.jpeg",import.meta.url)
 
 export default function Welcome(){
@@ -9,7 +11,12 @@ export default function Welcome(){
             <img className={classes.image} src={pic}></img>
             </div>
             <div className={classes.words}>
-            <h1 className={classes.title}>FOCUS UP</h1>
+            <Logo/>
+            <h1 className={classes.phrase}>Get stuck in</h1>
+            <h1 className={classes.para}>Join FOCUS-UP today</h1>
+            <div className={classes.signup}>
+            <Signup />
+            </div>
             </div>
             </div>
         </div>
