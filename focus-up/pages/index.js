a172c76ce52cfc68507d6123fd10618c205261e8
RICHARD STEPHEN - STUDENT <G00382880@atu.ie>
2023-01-23 14:41:12 +0000

    Added big logo at bottom of page

--- focus-up/pages/index.js
+++ focus-up/pages/index.js
@@ -1,7 +1,7 @@
 import classes from "../styles/Home.module.css"
 import Logo from "../components/Logo/logo"
+import LogoSmall from "../components/LogoSmall/logo"
 import Signup from "../components/WelcomeButtons/signup"
-import "reactjs-popup/dist/index.css"
 const pic = new URL("../images/study.jpeg",import.meta.url)
 
 export default function Welcome(){
@@ -13,14 +13,19 @@ export default function Welcome(){
             <img className={classes.image} src={pic}></img>
             </div>
             <div className={classes.words}>
-            <Logo/>
+            <LogoSmall/>
             <h1 className={classes.phrase}>Get stuck in</h1>
             <h1 className={classes.para}>Join FOCUS-UP today</h1>
             <div className={classes.signup}>
             <Signup/>
             </div>
-
             </div>
+            </div>
+            <div className={classes.logo}>
+                <div className={classes.inLogo}>
+                <h1>FOCUS UP</h1>
+                <Logo/>
+                </div>
             </div>
         </div>
     )
