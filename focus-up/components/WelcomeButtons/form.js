17c9b349c8d6192c92f8ff659d8f2e34df207df7
RICHARD STEPHEN - STUDENT <G00382880@atu.ie>
2023-01-23 14:54:01 +0000

    Added extra text to imply required fields

--- focus-up/components/WelcomeButtons/form.js
+++ focus-up/components/WelcomeButtons/form.js
@@ -3,7 +3,9 @@ import classes from "./buttons.module.css"
 export default function Form(){
     return(
         <div className={classes.container}>
+            <p className={classes.email}>Enter email *</p>
             <input className={classes.input} type="text" placeholder="Enter email..." required/>
+            <p className={classes.email}>Enter password *</p>
             <input className={classes.input} type="text" placeholder="Enter Password..." required/>
             <button className={classes.submit}><Link href = '/home'>Submit</Link></button>
         </div>
