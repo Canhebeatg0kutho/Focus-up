0b1af15dbb400705f93005390c463ed211ab25af
Canhebeatg0kutho <G00382880@atu.ie>
2023-01-24 18:12:10 +0000

    Connected to database

--- focus-up/server.js
+++ focus-up/server.js
@@ -2,6 +2,11 @@ const express = require("express")
 const { default: mongoose } = require("mongoose")
 const app = express()
 const Mongoose = require("mongoose")
-mongoose.connect('')
+mongoose.connect('mongodb+srv://RStephens:focusup@cluster0.huesiav.mongodb.net/?retryWrites=true&w=majority')
+
+const db = mongoose.connection
+db.on('error',(error)=> console.error(error))
+db.once('open',()=> console.error('Connected to database'))
+
 
 app.listen(3001,()=> console.log('Server Started'))
