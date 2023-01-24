ae7ed3a890b1ac84e183d018ec6951360ac712f9
Canhebeatg0kutho <G00382880@atu.ie>
2023-01-24 18:19:13 +0000

    Moved mongoDB link to env file and assigned it a variable

--- focus-up/server.js
+++ focus-up/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const express = require("express")
 const { default: mongoose } = require("mongoose")
 const app = express()
 const Mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://RStephens:focusup@cluster0.huesiav.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.DATABASE_URL)
 
 const db = mongoose.connection
 db.on('error',(error)=> console.error(error))
