6cc7fba3af9f7a511b170c1ebb5bba6e1a7912cd
Canhebeatg0kutho <G00382880@atu.ie>
2023-01-24 18:03:45 +0000

    Connected to nodejs server

--- focus-up/server.js
+++ focus-up/server.js
@@ -1,4 +1,7 @@
 const express = require("express")
+const { default: mongoose } = require("mongoose")
 const app = express()
+const Mongoose = require("mongoose")
+mongoose.connect('')
 
 app.listen(3001,()=> console.log('Server Started'))
