a4046a0ee4d19fcaa47bb64b0eb13920ba62f7e6
RICHARD STEPHEN - STUDENT <G00382880@atu.ie>
2023-01-27 12:07:19 +0000

    Installed ejs and added get request for routes

--- focus-up/server.js
+++ focus-up/server.js
@@ -12,8 +12,14 @@ db.on('error',(error)=> console.error(error))
 db.once('open',()=> console.error('Connected to database'))
 
 app.use(express.json())
+app.set("view engine","ejs")
 
 const userRouter = require('./Authentication/route')
 app.use('/users', userRouter)
+
+app.get("/",(req,res) => res.render("home"))
+app.get("/register",(req,res) => res.render("register"))
+app.get("/login",(req,res) => res.render("login"))
+
 
 app.listen(3000,()=> console.log('Server Started'))
