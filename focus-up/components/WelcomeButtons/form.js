ba57470da5a43832dae92ebdb9ed0fc8fff80d29
Canhebeatg0kutho <G00382880@atu.ie>
2023-02-07 19:52:26 +0000

    Removed div in form

--- focus-up/components/WelcomeButtons/form.js
+++ focus-up/components/WelcomeButtons/form.js
@@ -7,7 +7,7 @@ import axios from "axios"
 export default function Form(){
     const [username, setUser] = useState("")
     const [password, setPassword] = useState("")
-    const [isLogin, setLogin] = useState(false)
+    const [isRegister, setRegister] = useState(false)
 
 const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,19 +20,17 @@ const handleSubmit = (e) => {
         }
     }
     axios(configuration)
-    .then((result) => {console.log(result);})
-    .catch((error) => {console.log(error);})
+    .then((result) => {setRegister(true);})
+    .catch((error) => {error = new Error();})
 }
 
     return(
-        <div className={classes.container}>
-            {/* <Form onSubmit={handleSubmit}> */}
+            <Form onSubmit={handleSubmit} className={classes.container}>
             <h3 className={classes.email}>Enter username *</h3>
             <input className={classes.input} type="text" value={username} placeholder="Enter username..." onChange={(e) => setUser(e.target.value)} required />
             <h3 className={classes.email}>Enter password *</h3>
             <input className={classes.input} type="password" value={password} placeholder="Enter Password..." onChange={(e) => setPassword(e.target.value)} required/>
             <button onClick={handleSubmit} className={classes.submit}>Submit</button>
-            {/* </Form> */}
-        </div>
+            </Form>
     )
 }
