045c5220bf7f23e480d5dc4965eb3ee1f62bd1b4
Canhebeatg0kutho <G00382880@atu.ie>
2023-03-05 11:51:29 +0000

    Added fetching from user db and routing via username

--- focus-up/components/TextArea/text.js
+++ focus-up/components/TextArea/text.js
@@ -3,7 +3,7 @@ import classes from "./text.module.css"
 export default function Text(){
     return(
         <div className={classes.text}>
-        <textarea rows = "50" cols = "80" placeholder="Enter details here...">  </textarea>
+        <textarea rows = "50" cols = "80" placeholder="Enter details here..."> </textarea>
         </div>
     )
 }
--- focus-up/pages/notes/index.js
+++ focus-up/pages/notes/index.js
@@ -1,14 +1,30 @@
+import Link from 'next/link';
 import Nav from "../../components/nav"
 import Buttons from "../../components/Tabs/Buttons"
 import Text from "../../components/TextArea/text"
 
+export const getStaticProps = async () =>{
+  const res = await fetch("http://localhost:3000/users")
+  const data = await res.json()
 
-export default function Notes(){
+  return{
+    props: {users: data}
+  }
+}
+
+export default function Notes({users}){
     return(
   <div>
     <Nav/>
     <Buttons/>
     <Text/>
+    {users.map(user=> (
+      <Link href={'/notes/' + user.username} key={user.id}>
+      <a>
+        <h3>{user.username}</h3>
+      </a>
+      </Link>
+    ))}
   </div>
     )
 }
