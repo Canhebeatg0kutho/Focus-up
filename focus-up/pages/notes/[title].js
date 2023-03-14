ed770e93366082fec6e65f87db6d4d7c96c3ce3f
RICHARD STEPHEN - STUDENT <g00382880@atu.ie>
2023-03-14 14:28:57 +0000

    Added functionality of notes for different pages

--- focus-up/pages/notes/[title].js
+++ focus-up/pages/notes/[title].js
@@ -1,9 +1,10 @@
 import { useRouter } from "next/router"
 import Nav from "../../components/nav"
 import Buttons from "../../components/Tabs/Buttons"
-import classes from "../../components/Tabs/buttons.module.css"
 import Link from 'next/link';
 import Text from "../../components/TextArea/text"
+import classes from "./notes.module.css"
+
 
 
 export const getStaticPaths = async () =>{
@@ -32,37 +33,26 @@ console.log(data)
     }
   }
 
-//   const setNote = async() =>{
-//     const data = awaitfetch(
-//        "http://localhost:3000/notes/update",
-//        {
-//           method:'PATCH',
-//           headers:{
-//            'Content-Type': 'application/json',
-//           },
-//           body:JSON.stringify({
-           
-//           })
-//        }
-//     )
-// }
+
 
 export default function NotesDetail({notes}){
     return(
         <div>
         <Nav/>
-        <div className={classes.buttons}>
-        <button><Link href = '/tasks'> Tasks </Link></button>
-        <button><Link href = '/timer'> Timer </Link></button>
-        <button><Link href = '/notes'> Notes</Link></button>
+        <Buttons/>
         {notes.map(note =>(
             <h1 key={note.id}>{note.title} Notes</h1>
         ))}
-        </div>
+
         <div className={classes.text}>
-        <textarea rows = "50" cols = "80" placeholder="Enter details here..."> </textarea>
+        <textarea rows = "50" cols = "80" placeholder="Enter details here..."> 
+        {
+            notes.map(note =>(
+                `${note.note}`
+            ))
+        }
+        </textarea>
         </div>
-        <Text/>
         </div>
     )
 }
