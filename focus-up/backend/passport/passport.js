16ae5e4678e6dd6165d09b497e8ad8bb2236c49d
Canhebeatg0kutho <G00382880@atu.ie>
2023-04-07 14:38:44 +0100

    Added serialization and deserialization of users

--- focus-up/backend/passport/passport.js
+++ focus-up/backend/passport/passport.js
@@ -24,3 +24,17 @@ const verifyCallback = (username, password, done) => {
 }
 
 const strategy  = new LocalStrategy(customFields, verifyCallback);
+passport.use(strategy);
+
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((userId, done) => {
+    User.findById(userId)
+        .then((user) => {
+            done(null, user);
+        })
+        .catch(err => done(err))
+});
