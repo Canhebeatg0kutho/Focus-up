a5514fc2d1335e993519dc250c90b1ab5e0dfa59
Canhebeatg0kutho <G00382880@atu.ie>
2023-04-07 14:37:10 +0100

    Added validation for users

--- focus-up/backend/passport/passport.js
+++ focus-up/backend/passport/passport.js
@@ -1,1 +1,23 @@
 const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+const Users = require('../schema/User')
+
+const verifyCallback = (username, password, done) => {
+
+    Users.findOne({ username: username })
+        .then((user) => {
+
+            if (!user) { return done(null, false) }
+            
+            const isValid = validPassword(password, user.hash, user.salt);
+            
+            if (isValid) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        })
+        .catch((err) => {   
+            done(err);
+        });
+}
