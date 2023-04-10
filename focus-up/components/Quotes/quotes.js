04dc8ba331fc0a1ebba4b1ff776e2c9d293fbf41
Canhebeatg0kutho <G00382880@atu.ie>
2023-04-10 13:58:46 +0100

    Added youtube thumnails embedded to the video

--- focus-up/components/Quotes/quotes.js
+++ focus-up/components/Quotes/quotes.js
@@ -7,7 +7,6 @@ export default function Quotes(){
         <div className={classes.quotes}>
         <h1 className={classes.text}>“Learning is the only thing the mind never exhausts, never fears, and never regrets.” – Leonardo da Vinci.</h1>
         <h1 className={classes.text}>“Success is not final, failure is not fatal: it is the courage to continue that counts.” – Winston Churchill</h1>
-        <h1 className={classes.text}>“Failure is the opportunity to begin again more intelligently.” – Henry Ford</h1>
         <h1 className={classes.text}>“When you change your thoughts, remember to also change your world.”—Norman Vincent Peale</h1>
         </div>
     )
--- focus-up/pages/home/index.js
+++ focus-up/pages/home/index.js
@@ -2,6 +2,10 @@ import Nav from '../../components/nav';
 import classes from './home.module.css'
 import Buttons from '../../components/Tabs/Buttons';
 import Quotes from '../../components/Quotes/quotes';
+const pic = new URL("../../images/Organise.jpg",import.meta.url)
+const pic2 = new URL("../../images/Recall.jpg",import.meta.url)
+const pic3 = new URL("../../images/stanford.jpg",import.meta.url)
+
 
 export default function Home() {
   return (
@@ -10,6 +14,12 @@ export default function Home() {
   <h1 className={classes.title}>WELCOME TO FOCUS-UP!</h1>
   <Buttons/>
   <Quotes/>
+  <h1 className={classes.head}>Useful videos:</h1>
+  <div className={classes.links}>
+  <a href="https://youtu.be/XqdDMNExvA0" target="_blank"><img className={classes.thumbnail} src={pic} alt="study"></img></a>
+  <a href="https://youtu.be/MSq-KGj_cnY" target="_blank"><img className={classes.thumbnail} src={pic2} alt="study"></img></a>
+  <a href="https://youtu.be/ZYJ17UVhI30" target="_blank"><img className={classes.thumbnail} src={pic3} alt="study"></img></a>
+  </div>
   </div>
   )
 }
