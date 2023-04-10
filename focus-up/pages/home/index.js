b1e59b885cbdaf3cbec21b3be44f9fd1bc789f63
Canhebeatg0kutho <G00382880@atu.ie>
2023-04-10 12:09:10 +0100

    created quotes component

--- focus-up/pages/home/index.js
+++ focus-up/pages/home/index.js
@@ -1,6 +1,7 @@
 import Nav from '../../components/nav';
 import classes from './home.module.css'
 import Buttons from '../../components/Tabs/Buttons';
+import Quotes from '../../components/Quotes/quotes';
 
 export default function Home() {
   return (
@@ -8,12 +9,7 @@ export default function Home() {
   <Nav/>
   <h1 className={classes.title}>WELCOME TO FOCUS-UP!</h1>
   <Buttons/>
-  <div className={classes.quotes}>
-  <h1 className={classes.text}>“Learning is the only thing the mind never exhausts, never fears, and never regrets.” – Leonardo da Vinci.</h1>
-  <h1 className={classes.text}>“Success is not final, failure is not fatal: it is the courage to continue that counts.” – Winston Churchill</h1>
-  <h1 className={classes.text}>“Failure is the opportunity to begin again more intelligently.” – Henry Ford</h1>
-  <h1 className={classes.text}>“When you change your thoughts, remember to also change your world.”—Norman Vincent Peale</h1>
-  </div>
+  <Quotes/>
   </div>
   )
 }
