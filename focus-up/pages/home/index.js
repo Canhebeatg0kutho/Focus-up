d8f34d69f4e6f02c66002a831761d2888f0cf60d
Canhebeatg0kutho <G00382880@atu.ie>
2023-04-30 16:41:58 +0100

    change localhost to ip

--- focus-up/pages/home/index.js
+++ focus-up/pages/home/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await axios.get("http://localhost:3000/users/protected-route", {
+        const res = await axios.get("http://54.83.151.50:3000/users/protected-route", {
           withCredentials: true,
         });
         setData(res.data);
