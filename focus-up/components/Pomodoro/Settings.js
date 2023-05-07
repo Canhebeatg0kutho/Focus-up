9bc472829d21dcd8c4b5fb1bd49f8d7f47ff4965
Canhebeatg0kutho <G00382880@atu.ie>
2023-05-07 11:21:20 +0100

    Made popup more compact

--- focus-up/components/Pomodoro/Settings.js
+++ focus-up/components/Pomodoro/Settings.js
@@ -1,60 +1,65 @@
-import { useState } from "react"
-import classes from "./pomo.module.css"
+import { useState } from "react";
+import classes from "./pomo.module.css";
 
+export default function Settings() {
+  const [title, setTitle] = useState("Work");
+  const [newSeconds, setSeconds] = useState("");
+  const [newMinutes, setMinutes] = useState("");
+  const [currentState, changeState] = useState(true);
 
-
-export default function Settings(){
-    const[title,setTitle] = useState("Work")
-    const[newSeconds,setSeconds]= useState('')
-    const[newMinutes,setMinutes]= useState('')
-    const[currentState,changeState]= useState(true)
-  
-
-    const editTimer = async() => {
-     await fetch("http://localhost:3000/timer/edit", {
+  const editTimer = async () => {
+    await fetch("http://localhost:3000/timer/edit", {
       method: "PATCH",
       Accept: "application/json",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        title:title,
+        title: title,
         minutes: newMinutes,
-        seconds: newSeconds
+        seconds: newSeconds,
       }),
     });
-}
+  };
 
-const handleClick = () =>{
-  changeState(!currentState)
-    currentState ? setTitle("Work") : setTitle("Break")
+  const handleClick = () => {
+    changeState(!currentState);
+    currentState ? setTitle("Work") : setTitle("Break");
+  };
+  return (
+    <div>
+      <div className={classes.form}>
+        <p className={classes.settingsTitle}>Settings for {title}</p>
+        <button className={classes.change} onClick={() => handleClick()}>
+          {" "}
+          Change settings
+        </button>
+        <div className={classes.setTimes}>
+            <input
+              type="text"
+              className={classes.inputMin}
+              placeholder="Enter new minutes"
+              value={newMinutes}
+              onChange={(e) => setMinutes(e.target.value)}
+            />
 
-}
-    return(
-        <div>
-         <div className={classes.form}>
-         <p className={classes.settingsTitle}>Settings for {title}</p>
-         <button className={classes.change} onClick={() => handleClick()}> Change settings</button>
-          <input
-            type="text"
-            className={classes.input}
-            placeholder="Enter new minutes"
-            value={newMinutes}
-            onChange={(e) => setMinutes(e.target.value)}
-          />
-          <input
-            type="text"
-            className={classes.input}
-            placeholder="Enter new seconds"
-            value={newSeconds}
-            onChange={(e) => setSeconds(e.target.value)}
-          />
-           <button className={classes.submit} onClick={async () => {
-              editTimer( { title: title, minutes:newMinutes,seconds:newSeconds });
-            }}
-          >Submit</button>
-         </div>
-
+            <input
+              type="text"
+              className={classes.inputSec}
+              placeholder="Enter new seconds"
+              value={newSeconds}
+              onChange={(e) => setSeconds(e.target.value)}
+            />
         </div>
-    )
+        <button
+          className={classes.submit}
+          onClick={async () => {
+            editTimer();
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
